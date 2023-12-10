8cd84660d701e0f89f96597906d3233b427d0f15
SparrowARK <karkiaaditya2@gmail.com>
2023-12-10 20:07:09 +0545

    Movie site api implementation and completed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name :{props.name}</h1>
-    <h2>Last Name : {props.lastName}</h2>
-    <h2>Age : {props.age}</h2>
-    </>
-    
-  )
-}
+//cfe151f4
+const API_URL = 'http://www.omdbapi.com?apikey=cfe151f4'
+
+// const movie1 = {
+//     "Title": "Superman, Spiderman or Batman",
+//     "Year": "2011",
+//     "imdbID": "tt2084949",
+//     "Type": "movie",
+//     "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+// }
 
 const App = () => {
-  const [counter, setCounter] =useState(0);
-  const name = 'Aaditya';
-  const isNameShowing = true;
-  
-  useEffect(() => {
-    alert('You have changes the counter to ' + counter);
-  }, [counter]);
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
-  return (
-    <div className="App">
-      <Person 
-        name={'Aaditya'} 
-        lastName={'Karki'} 
-        age={'23'}
-      />
-      <Person name={'Mary'} lastName={'Doe'} age={'18'}/>
-      <Person name={'Jane'} lastName={'Doe'} age={'300'}/>
-      <h1>Hello, {isNameShowing ? name : 'someone'}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    },[]);
+    return(
+        <div className="app">
+            <h1>Movie Sansar</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies" 
+                    value={searchTerm} 
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                  src={SearchIcon}
+                  alt = "search"
+                  onClick = {() => searchMovies(searchTerm)}
+                />
+            </div>
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                       {movies.map((movie)=> (
+                        <MovieCard movie={movie}/>
+                       ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2> No movies Found</h2>
+                    </div>
+                )
+            }     
+        </div>
+    );
 }
 
 export default App;
